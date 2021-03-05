4a24673b2122f768d68308698037f73e9a338ba2
F1nqer <RetroLOL2>
2021-03-05 16:39:25 +0300

    Добавьте файлы проекта.

---
+++ KaspiConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaspiConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
