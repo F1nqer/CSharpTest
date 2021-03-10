ec0b7ede81a679456f7fcf9103561f38fb78ddcf
F1nqer <RetroLOL2>
2021-03-10 21:41:04 +0300

    third task

---
+++ OopStorage/StorageClasses/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopStorage.StorageClasses
+{
+    class Employee
+    {
+        public string FullName;
+        public string Position;
+    }
+}
---
+++ OopStorage/StorageClasses/PeaceProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopStorage.StorageClasses
+{
+    class PeaceProduct : Product
+    {
+        public string Name { get; set; }
+        public int SKU { get; set; }
+        public string Definition { get; set; }
+        public int Price { get; set; }
+    }
+}
